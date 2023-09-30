45adc632a40c420290d178677f466e56383398bb
Igor <Comedy@Veryfunny.com>
2023-09-30 17:55:57 +0300

    first python project

---
+++ file.py
@@ -0,0 +1,12 @@
+
+print("введите пароль")
+r=1456
+num=int(input())
+if r == num:
+    print("пароль верный")
+else:
+    print("пароль неверный")
+side = input("введите сторону квадрата")
+side =int(side)
+s = side * side
+print("Площадь квадрата равна"+str(s))
