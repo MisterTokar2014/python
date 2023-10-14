a9c38c0b7c0816e60fe01e221ad6114540e00311
Igor <Comedy@Veryfunny.com>
2023-10-14 17:27:40 +0300

    add conditions

---
+++ passwod picer/hgh.py
@@ -0,0 +1,9 @@
+login = input("Введите логин")
+password = input("Введите пароль")
+
+right_puss = "2222"
+log = "Kostya"
+if login == log and password == right_puss:
+    print("Вы вошли")
+else:
+    print("Логин неверный и вы не вошли")
