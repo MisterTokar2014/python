36193e9bda63d25dc7b4b07de7925296848af53c
Igor <Comedy@Veryfunny.com>
2024-01-13 17:37:47 +0300

    add loginpass

--- y.py
+++ y.py
@@ -1,3 +1,4 @@
+import time
 def sing_up():
     login = input('придумайте логин: ')
     password1 = input('придумайте пароль')
@@ -8,25 +9,56 @@ def sing_up():
         print(f"Пользователь{login} успешно заригестрирован")
     else:
         print("пароли не схожи. Проверьте ещё раз иначе Капибара с мандарином взорвёт ваш компьютер")
-        
-        
-        
+    
+
+def sing_in():
+    login = input("введите логин: ")
+    password = input("введите пароль: ")  
+    login_list = read_list("logins.txt") 
+    password_list = read_list("passwords.txt")    
+    if login in login_list: #если логин в списке логинов
+        login_str = login_list.index(login)# находим его индекс
+        true_password = password_list[login_str]# берём пароль с таким же индексом
+        if password == true_password:
+            print("вы успешно вошли в систему")
+            return True
+        else:
+            print("пароль неправильный")
+            return False
+    else:
+        print("пользователя с таким логином не существует")
+        return False
+            
         
         
 def write_to_file(filename, text):
-    with open('y', mode='a', encoding='UTF-8') as file:
-        file.write()
+    with open(filename, mode='a', encoding='UTF-8') as file:
+        file.write(text)
+
+def read_list(filename):
+    with open(filename, mode="r", encoding="UTF-8") as file:
+        text = file.read().split("\n")
+        return text
+
+
+
         
-        
-        
+access = False         
 while True:
-    print('Это прграмма Капибара с мандарином, и вот что она умеет')
-    print('1-зарегестрировоться')
-    print('2-выйдти из программыы')
-    acsh = input("введите номер действия:")
-    if acsh == '1':
-        sing_up()
-    if acsh == '2':
-        print("спасибо за использование программы Капибара с мандарином")
-        break
+    if not access:
+        print('Это прграмма Капибара с мандарином, и вот что она умеет')
+        print('1-зарегестрироваться')
+        print("2-войти в систему")
+        print('3-выйдти из программыы')
+        acsh = input("введите номер действия:")
+        if acsh == '1':
+            sing_up()
+        elif acsh == '2':
+            access = sing_in()
+            
+        if acsh == '3':
+            print("спасибо за использование программы Капибара с мандарином")
+            break
+    elif access:
+        print("вы в системе!")
     input("нажмите энтер, что-бы продолжить")
